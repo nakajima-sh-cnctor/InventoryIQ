065d73c613122b0d5bd13e144156d5f5c1f39cf8
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-02-14 18:35:23 +0900

    Configure Vuetify integration in Nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    '@mdi/font/css/materialdesignicons.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    plugins: [vuetify({ autoImport: true })],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  plugins: [
+    '~/plugins/vuetify.ts',
+  ],
+});
