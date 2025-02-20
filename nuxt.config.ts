0b43396fcf3b5aeaeafc19940fa2500a15de7806
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-02-20 11:17:31 +0900

    Add Vuetify integration with configuration and styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,31 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint',
+    (_options, nuxt): void => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
   devtools: { enabled: true },
+  css: [
+    '@mdi/font/css/materialdesignicons.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
   compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [vuetify({ autoImport: true })],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   eslint: {
     config: {
       stylistic: true,
